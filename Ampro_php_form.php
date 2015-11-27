b47cdcc664bb3c80fd67db9d6159e223f624fd03
weipanchang <weipanchang@hotmail.com>
2015-11-27 14:21:58 -0800

    third commit

--- Ampro_php_form.php
+++ Ampro_php_form.php
@@ -16,7 +16,7 @@ $commenterror = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    if (empty($_POST["barcode"])) {
-     $nameErr = "Barcode is required";
+     $barcodeerror = "Barcode is required";
    } else {
      $barcode = test_input($_POST["barcode"]);
    }
@@ -39,28 +39,13 @@ function test_input($data) {
 ?>
 
 <h2>Ampro System PCB Check in/out</h2>
-<p><span class="error">* Please Scan the Barcod *</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-   Barcode:  <input type="text" name="barcode">
+<form method = "post" action="">
+<p><span class="error">* Please Scan the Barcode *</span></p>
+   Barcode:  <input type="text" name="barcode" value="<?php echo $barcode;?>">
    <span class="error"> <?php echo $barcodeerror;?></span>
    <br><br>
-<!--   E-mail: <input type="text" name="email">
-   <span class="error"> <?php echo $emailErr;?></span>
    <br><br>
-   Website: <input type="text" name="website">
-   <span class="error"><?php echo $websiteErr;?></span>
-   <br><br>
-   Comment: <textarea name="Note from Last Location" rows="6" cols="40"></textarea>
-   <br><br>
-   Action:
-   <input type="radio" name="Action" value="Check In">Check In
-   <input type="radio" name="Action" value="Check Out">Check Out
-   <span class="error"> <?php echo $genderErr;?></span>
-   <br><br>
-   <input type="submit" name="submit" value="Submit">
--->
-</form>
-
+</form>   
 <?php
 echo "<h2>Your Input:</h2>";
 echo $barcode;
@@ -68,6 +53,13 @@ echo "<br>";
 echo $comment;
 echo "<br>";
 ?>
+
+<form method="post" action="Ampro_process.php" >
+   <input type="hidden" name="barcode"
+     value="<?php echo $_POST['barcode']; ?>">
+   <input type="submit" name="submit" value="submit">
+</form>
+
 
 </body>
 </html>
---
+++ Ampro_process.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>
+
+<?php
+echo $_POST['barcode'];
+$barcode = $_POST['barcode'];
+echo $barcode;
+?>
+
+</body>
+</html>
+
+
